96823d63762459196ddd2dbe8d71c5b59a286c97
eunki0916 <eungi0916@naver.com>
2023-03-19 14:58:36 +0900

    뉴스뷰어

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from "react";
+import Categories from "./components/Categories";
+import { Routes, Route } from "react-router-dom";
+import NewsList from "./components/NewsList";
+import NewsPage from "./pages/NewsPage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<NewsPage />}></Route>
+      <Route path="/:category" element={<NewsPage />}></Route>
+    </Routes>
   );
-}
+};
 
 export default App;
